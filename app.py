f3eab9808a50ce40cabf0eda9d3f00d9be4ca501
KanhaKanhaiya <thebig6project@gmail.com>
2022-06-09 14:47:38 +0530

    Update app.py
    Add docstrings and change function name

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+"""Entrypoint for backend website"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home_page():
+    """Returns Home Page"""
     return "<h1>The big6 Project</h1>"
